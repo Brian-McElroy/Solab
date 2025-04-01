fe73b83cdc945827cca3dcc1a5af1744f99cde5a
Brian-McElroy <bamcelroy@gmail.com>
2025-04-01 20:03:56 +0100

    admin tools

--- SogoTools/Common.js
+++ SogoTools/Common.js
@@ -1,2 +1,2 @@
-//const url = "http://localhost:7005/"
-const url = "https://solab-access-438f80e69184.herokuapp.com/"
+const url = "http://localhost:7005/"
+//const url = "https://solab-access-438f80e69184.herokuapp.com/"
---
+++ SogoTools/ManageArtists.js
@@ -0,0 +1,114 @@
+
+
+function PopulateArtistData()
+{
+    //location.reload();
+
+    let key = document.getElementById("AdminKeyField").value;
+
+    $.get(
+        url + "GetAllArtistData",
+        {key:key},
+        function(data)
+        {
+            let artistlist = document.getElementById("artistList");
+
+            for (let i = artistlist.children.length -1; i >0  ; i--)
+            {
+                artistlist.children[i].remove();               
+            }
+
+            for (const element of data)
+            {
+                //console.log(JSON.stringify(element));
+                let clone = artistlist.firstElementChild.cloneNode(true);
+                clone.style.display = "block";  
+                clone.children[0].innerHTML = "Name: " + element.name + "  ID: " + element.id + "  Invite code: " + element.inviteCode;
+                clone.children[1].innerHTML = "Friends:"
+                for (const friend of element.friends) clone.children[1].innerHTML += "   " +friend;
+
+                artistlist.appendChild(clone); 
+            }
+        }
+    );
+}
+
+function RemoveArtist()
+{
+    let key = document.getElementById("AdminKeyField").value;
+    let id = document.getElementById("removeid").value;
+
+    if(!id) return;
+
+    document.getElementById("removeid").value = "";
+
+    $.get(
+        url + "RemoveAnArtist",
+        {key:key,id:id}
+    )
+    .done(function(data) {
+        document.getElementById("error").innerHTML =  data;
+        PopulateArtistData();
+    })
+    .fail(function(jqXHR, textStatus, errorThrown) {
+        document.getElementById("error").innerHTML =  textStatus +" "+errorThrown;
+    })
+    .always(function() {
+    });
+}
+
+function AddAFriend()
+{
+    let key = document.getElementById("AdminKeyField").value;
+    let id1 = document.getElementById("friendshipA").value;
+    let id2 = document.getElementById("friendshipB").value;
+
+    if(!id1) return;
+    if(!id2) return;
+
+    document.getElementById("friendshipA").value = "";
+    document.getElementById("friendshipB").value = "";
+
+    $.get(
+        url + "CreateFriendship",
+        {key:key,id1:id1,id2:id2}
+    )
+    .done(function(data) {
+        document.getElementById("error").innerHTML =  data;
+        PopulateArtistData();
+    })
+    .fail(function(jqXHR, textStatus, errorThrown) {
+        document.getElementById("error").innerHTML =  textStatus +" "+errorThrown;
+    })
+    .always(function() {
+    });
+}
+
+function RemoveAFriend()
+{
+    let key = document.getElementById("AdminKeyField").value;
+    let id1 = document.getElementById("removefriendshipA").value;
+    let id2 = document.getElementById("removefriendshipB").value;
+
+    if(!id1) return;
+    if(!id2) return;
+
+    document.getElementById("removefriendshipA").value = "";
+    document.getElementById("removefriendshipB").value = "";
+
+    $.get(
+        url + "RemoveFriendship",
+        {key:key,id1:id1,id2:id2}
+    )
+    .done(function(data) {
+        document.getElementById("error").innerHTML =  data;
+        PopulateArtistData();
+    })
+    .fail(function(jqXHR, textStatus, errorThrown) {
+        document.getElementById("error").innerHTML =  textStatus +" "+errorThrown;
+    })
+    .always(function() {
+    });
+}
+
+document.getElementById("GetArtistData").onclick =PopulateArtistData;

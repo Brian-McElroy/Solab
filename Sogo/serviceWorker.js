90f5c9d278bd9671025090c752c717c0c3dc9deb
Brian-McElroy <bamcelroy@gmail.com>
2025-04-02 17:52:55 +0100

    started PWA

--- Sogo/serviceWorker.js
+++ Sogo/serviceWorker.js
@@ -1,14 +1,14 @@
-const staticSogo = "sogo-site-v1"
+const staticDevCoffee = "dev-coffee-site-v1"
 const assets = [
   "/",
   "/index.html",
-  "/style.css",
-  //"/images/coffee1.jpg",
+  "/css/style.css",
+  "/js/app.js",
 ]
 
 self.addEventListener("install", installEvent => {
   installEvent.waitUntil(
-    caches.open(staticSogo).then(cache => {
+    caches.open(staticDevCoffee).then(cache => {
       cache.addAll(assets)
     })
   )
---
+++ Sogo/setupPWA.js
@@ -0,0 +1,35 @@
+
+
+function addMetaTag(name, content) {
+    const meta = document.createElement("meta");
+    meta.name = name;
+    meta.content = content;
+    document.head.appendChild(meta);
+}
+
+function addLinkTag(rel, href) {
+    const link = document.createElement("link");
+    link.rel = rel;
+    link.href = href;
+    document.head.appendChild(link);
+}
+
+// Add manifest
+addLinkTag("manifest", "manifest.json");
+
+// Add Apple Touch Icons
+const iconSizes = [72, 96, 128, 144, 152, 192, 384, 512];
+iconSizes.forEach(size => {
+    addLinkTag("apple-touch-icon", `images/icons/icon-${size}x${size}.png`);
+});
+
+// Add theme colors
+addMetaTag("apple-mobile-web-app-status-bar", "#db4938");
+addMetaTag("theme-color", "#db4938");
+addMetaTag("theme-color", "#db4938");
+
+
+
+
+
+

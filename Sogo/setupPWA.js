5258918f7953882159aca973339788a752fe6537
Brian-McElroy <bamcelroy@gmail.com>
2025-04-02 17:57:26 +0100

    doop

--- Sogo/setupPWA.js
+++ Sogo/setupPWA.js
@@ -24,8 +24,7 @@ iconSizes.forEach(size => {
 });
 
 // Add theme colors
-addMetaTag("apple-mobile-web-app-status-bar", "#db4938");
-addMetaTag("theme-color", "#db4938");
+addMetaTag("apple-mobile-web-app-status-bar", "#000000");
 addMetaTag("theme-color", "#db4938");
 
 
